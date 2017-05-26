034f831797e6fadd9825861ea0533c3aa58e9f81
Thomas Cheung <cheungth25@gmail.com>
2017-05-26 08:54:24 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-  array.join(", ")
+  if array.length == 1
+    string = array[0]
+  elsif array.length == 2
+    string = array.join(" and ")
+  else
+    array[-1] = "and #{array[-1]}"
+    string = array.join(", ")
+  end
+  return string
 end
